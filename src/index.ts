791929037ed71421b593698bf67061c95d902f29
Yusuke Wada <yusuke@kamawada.com>
2022-12-21 22:33:09 +0900

    fixed path

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,9 @@ import view from './index.html'
 import { serveStatic } from 'hono/serve-static.module'
 
 const app = new Hono()
-
 app.get('/', (c) => c.html(view))
-app.route('/api', api)
 
-app.use('*', serveStatic({ root: './' }))
+app.route('/api', api)
+app.get('/build/*', serveStatic({ root: './' }))
 
 export default app
