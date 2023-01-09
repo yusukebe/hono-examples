357b93f8e1d453c9ef223fd20f700c82f7a54735
Yusuke Wada <yusuke@kamawada.com>
2023-01-09 21:18:48 +0900

    structure

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/serve-static.module'
-import ssrRoute from './ssr'
+import { ssr } from './ssr/middleware'
 import api from './api'
+import App from './App'
 
 const app = new Hono()
 app.route('/api', api)
 app.get('/assets/*', serveStatic({ root: './public' }))
 
-app.route('/', ssrRoute)
+app.get('*', ssr(App))
 
 export default app
--- src/ssr/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono'
-import { ssr } from './middleware'
-
-import App from '../App'
-
-const routes = new Hono()
-routes.get('*', ssr(App))
-
-export default routes
